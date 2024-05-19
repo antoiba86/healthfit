f6ca57e275af2cbf14c742678622c4e6ef795a27
Antonio Ibáñez <antoiba86@gmail.com>
2024-05-19 19:15:46 +0200

    feat: Create migration files for users and sessions

---
+++ src/Infrastructure/Common/Migration/Doctrine/Version20240519171237.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240519171237 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sessions (id INT NOT NULL, activity_id INT DEFAULT NULL, user_id INT DEFAULT NULL, tracking_init_date DATETIME NOT NULL, tracking_finish_date DATETIME NOT NULL, tracking_elapsed_time_value INT NOT NULL, tracking_elapsed_time_unit VARCHAR(255) NOT NULL, tracking_distance_value INT NOT NULL, tracking_distance_unit VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_9A609D1381C06096 (activity_id), INDEX IDX_9A609D13A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT NOT NULL, height INT NOT NULL, weight INT NOT NULL, age INT NOT NULL, distance_goal_value INT NOT NULL, distance_goal_unit VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE sessions ADD CONSTRAINT FK_9A609D1381C06096 FOREIGN KEY (activity_id) REFERENCES activities (id)');
+        $this->addSql('ALTER TABLE sessions ADD CONSTRAINT FK_9A609D13A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sessions DROP FOREIGN KEY FK_9A609D1381C06096');
+        $this->addSql('ALTER TABLE sessions DROP FOREIGN KEY FK_9A609D13A76ED395');
+        $this->addSql('DROP TABLE sessions');
+        $this->addSql('DROP TABLE users');
+    }
+}
